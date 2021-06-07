6e09b28d39db0e8bb2d6339fd4af2f0d70cc9f70
Tim Muller <muller.tim@outlook.com>
2021-06-07 21:05:44 +0200

    Finished Mobile Nav | Started Main

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let burger = document.getElementById("burger");
 let nav = document.getElementById("nav");
+let mainNavLinks = document.getElementsByClassName("mainNavLinks");
 
-window.addEventListener("click", () => {
+burger.addEventListener("click", () => {
   if (nav.className == "open") {
     nav.className = "";
     burger.setAttribute("src", "./images/icon-hamburger.svg");
@@ -10,3 +11,20 @@ window.addEventListener("click", () => {
     burger.setAttribute("src", "./images/icon-close.svg");
   }
 });
+
+for (let i = 0; i < mainNavLinks.length; i++) {
+  const linkNode = mainNavLinks[i];
+
+  linkNode.addEventListener("click", (e) => {
+    e.preventDefault();
+    let ul = linkNode.nextElementSibling;
+
+    if (ul.className == "open") {
+      linkNode.classList.remove("rotate");
+      ul.className = "";
+    } else {
+      linkNode.classList.add("rotate");
+      ul.className = "open";
+    }
+  });
+}
